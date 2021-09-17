0bbaa202781f233800c8c3839421f555f63195ff
simplerick <SimpleRick@yandex.ru>
2021-09-18 02:22:36 +0300

    lint errors

--- homework1/way1/main.go
+++ homework1/way1/main.go
@@ -1,51 +1,46 @@
 package main
 
 import (
-  "fmt"
-  "strings"
+	"fmt"
+	"strings"
 )
 
 type Args = map[string]int
 
-
-func get_line(num_line int, char int, size int) string {
-  s := make([]string, size)
-  for i := 0; i < size; i ++ {
-      if (i == num_line) || (i == size-1-num_line) {
-        s[i] = string(char)
-      } else {
-        s[i] = " "
-      }
-  }
-  return strings.Join(s, "")
+func getLine(num_line int, char int, size int) string {
+	s := make([]string, size)
+	for i := 0; i < size; i++ {
+		if (i == num_line) || (i == size-1-num_line) {
+			s[i] = string(rune(char))
+		} else {
+			s[i] = " "
+		}
+	}
+	return strings.Join(s, "")
 }
 
-
-func draw_sandglass(char int, size int, color int) {
-  edge_line := strings.Repeat(string(char), size)
-  fmt.Printf("\033[%dm", color)
-  fmt.Println(edge_line)
-  for i := 1; i < size-1; i ++ {
-    fmt.Println(get_line(i, char, size))
-  }
-  fmt.Println(edge_line)
+func drawSandglass(char int, size int, color int) {
+	edge_line := strings.Repeat(string(char), size)
+	fmt.Printf("\033[%dm", color)
+	fmt.Println(edge_line)
+	for i := 1; i < size-1; i++ {
+		fmt.Println(getLine(i, char, size))
+	}
+	fmt.Println(edge_line)
 }
-
 
 func sandglass(args Args) {
-  def_args := Args{
-    "char" : 'X',
-    "size" : 15,
-    "color" : 30,
-  }
-  for k, v := range args {
-    def_args[k] = v
-  }
-  draw_sandglass(def_args["char"], def_args["size"], def_args["color"])
+	def_args := Args{
+		"char":  'X',
+		"size":  15,
+		"color": 30,
+	}
+	for k, v := range args {
+		def_args[k] = v
+	}
+	drawSandglass(def_args["char"], def_args["size"], def_args["color"])
 }
 
-
-
 func main() {
-  sandglass(Args{"color" : 35, "size": 11})
+	sandglass(Args{"color": 35, "size": 11})
 }
--- homework1/way2/main.go
+++ homework1/way2/main.go
@@ -1,71 +1,66 @@
 package main
 
 import (
-  "fmt"
-  "strings"
+	"fmt"
+	"strings"
 )
 
 type Arg = map[string]int
 type Opt func(a *Arg)
 
 func char(x int) Opt {
-  return func(a *Arg) {
-    (*a)["char"] = x
-  }
+	return func(a *Arg) {
+		(*a)["char"] = x
+	}
 }
 
 func size(x int) Opt {
-  return func(a *Arg) {
-    (*a)["size"] = x
-  }
+	return func(a *Arg) {
+		(*a)["size"] = x
+	}
 }
 
 func color(x int) Opt {
-  return func(a *Arg) {
-    (*a)["color"] = x
-  }
+	return func(a *Arg) {
+		(*a)["color"] = x
+	}
 }
 
-
-func get_line(num_line int, char int, size int) string {
-  s := make([]string, size)
-  for i := 0; i < size; i ++ {
-      if (i == num_line) || (i == size-1-num_line) {
-        s[i] = string(char)
-      } else {
-        s[i] = " "
-      }
-  }
-  return strings.Join(s, "")
+func getLine(num_line int, char int, size int) string {
+	s := make([]string, size)
+	for i := 0; i < size; i++ {
+		if (i == num_line) || (i == size-1-num_line) {
+			s[i] = string(rune(char))
+		} else {
+			s[i] = " "
+		}
+	}
+	return strings.Join(s, "")
 }
 
-
-func draw_sandglass(char int, size int, color int) {
-  edge_line := strings.Repeat(string(char), size)
-  fmt.Printf("\033[%dm", color)
-  fmt.Println(edge_line)
-  for i := 1; i < size-1; i ++ {
-    fmt.Println(get_line(i, char, size))
-  }
-  fmt.Println(edge_line)
+func drawSandglass(char int, size int, color int) {
+	edge_line := strings.Repeat(string(char), size)
+	fmt.Printf("\033[%dm", color)
+	fmt.Println(edge_line)
+	for i := 1; i < size-1; i++ {
+		fmt.Println(getLine(i, char, size))
+	}
+	fmt.Println(edge_line)
 }
-
 
 func sandglass(opts ...Opt) {
-  args := Arg{
-    "char" : 'X',
-    "size" : 15,
-    "color" : 30,
-  }
-  for _, opt := range opts {
-    opt(&args)
-  }
-  draw_sandglass(args["char"], args["size"], args["color"])
+	args := Arg{
+		"char":  'X',
+		"size":  15,
+		"color": 30,
+	}
+	for _, opt := range opts {
+		opt(&args)
+	}
+	drawSandglass(args["char"], args["size"], args["color"])
 }
 
-
-
 func main() {
-  sandglass(color(34), size(5))
-  sandglass(char(10))
+	sandglass(color(34), size(5))
+	sandglass(char('X'))
 }
