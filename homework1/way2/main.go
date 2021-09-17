9e863f79bf95a063ef181b8429428bb3b3531da9
simplerick <SimpleRick@yandex.ru>
2021-09-17 16:09:28 +0300

    2 ways

---
+++ homework1/way2/main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+  "fmt"
+  "strings"
+)
+
+type Arg = map[string]int
+type Opt func(a *Arg)
+
+func char(x int) Opt {
+  return func(a *Arg) {
+    (*a)["char"] = x
+  }
+}
+
+func size(x int) Opt {
+  return func(a *Arg) {
+    (*a)["size"] = x
+  }
+}
+
+func color(x int) Opt {
+  return func(a *Arg) {
+    (*a)["color"] = x
+  }
+}
+
+
+func get_line(num_line int, char int, size int) string {
+  s := make([]string, size)
+  for i := 0; i < size; i ++ {
+      if (i == num_line) || (i == size-1-num_line) {
+        s[i] = string(char)
+      } else {
+        s[i] = " "
+      }
+  }
+  return strings.Join(s, "")
+}
+
+
+func draw_sandglass(char int, size int, color int) {
+  edge_line := strings.Repeat(string(char), size)
+  fmt.Printf("\033[%dm", color)
+  fmt.Println(edge_line)
+  for i := 1; i < size-1; i ++ {
+    fmt.Println(get_line(i, char, size))
+  }
+  fmt.Println(edge_line)
+}
+
+
+func sandglass(opts ...Opt) {
+  args := Arg{
+    "char" : 'X',
+    "size" : 15,
+    "color" : 30,
+  }
+  for _, opt := range opts {
+    opt(&args)
+  }
+  draw_sandglass(args["char"], args["size"], args["color"])
+}
+
+
+
+func main() {
+  sandglass(color(34), size(5))
+}
